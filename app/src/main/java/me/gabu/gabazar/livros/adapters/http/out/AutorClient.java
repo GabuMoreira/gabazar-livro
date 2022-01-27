61b34a6401235f92030268ab0a79cfe3db28f655
Gabriel Moreira <joaogmrodrigues@outlook.com>
2022-01-27 14:45:15 -0300

    🚑️ Fix + Openfeign

--- app/src/main/java/me/gabu/gabazar/livros/adapters/http/out/AutorClient.java
+++ app/src/main/java/me/gabu/gabazar/livros/adapters/http/out/AutorClient.java
@@ -1,8 +1,14 @@
 package me.gabu.gabazar.livros.adapters.http.out;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import me.gabu.gabazar.livros.adapters.http.out.dto.AutorDTO;
 
+@FeignClient(value = "AutorClient", url = "http://localhost")
 public interface AutorClient {
 
-    AutorDTO getAutor(String editoraID);
+    @GetMapping("/autore/{autorID}")
+    AutorDTO getAutor(@PathVariable("autorID") String autorID);
 }
